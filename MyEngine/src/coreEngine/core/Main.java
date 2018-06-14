f5e0dfd14dd3b3f01c47eb74460beb18cb59172f
Graham <Graham@Master-PC>
2018-06-13 20:59:39 -0400

    simple changes

--- MyEngine/src/coreEngine/core/Main.java
+++ MyEngine/src/coreEngine/core/Main.java
@@ -5,14 +5,17 @@ import util.loaders.LODLoader;
 
 public class Main {
 
-	public static void main(String[] args) {				
-		Window window = new Window("Test Engine 2.0", 1280, 16, 9);
-		Game game = new Game(window);
+	public static void main(String[] args) {
+		
+		
+		
+		
+		Window window = new Window("Test Engine 2.0", 1280, 16, 9);		
 		int[][] data = LODLoader.loadLOD("terrain.cfg");
 		for(int i = 0; i < data.length; i++){
 			System.out.println("LOD " + i + ": " + data[i][0] + "		" + data[i][1]);
-		}
-		
+		}				
+		Game game = new Game(window);
 		game.start();
 	}
 }
